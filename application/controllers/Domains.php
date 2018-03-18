ade6a686953ef084293269d3198a5b6635470d8a
Unknown <t3i.ngo@gmail.com>
2018-03-18 09:26:16 +0100

    d

--- application/controllers/Domains.php
+++ application/controllers/Domains.php
@@ -3,14 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Domains extends CI_Controller {
 
+    // public function index()
+    // {
+    //     $this->load->model('Domain_model');
+    //     $tab = $this->domain_model->domainData();
+    //     $return = array('code' => 200,
+    //         'message' => 'success',
+    //         'dat' => $tab);
+    //     echo json_encode($return, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    // }
+
     public function index()
     {
-        $this->load->model('Domain_model');
-        $tab = $this->domain_model->domainData();
-        $return = array('code' => 200,
-            'message' => 'success',
-            'dat' => $tab);
-        echo json_encode($return, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+        echo 'test';
     }
 
     public function help()
